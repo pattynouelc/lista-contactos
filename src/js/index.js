53e10810acea56392b1b3f2b55a8bf0d415380ee
pattynouelc <141960475+pattynouelc@users.noreply.github.com>
2024-10-19 15:38:04 +0000

    tarea en proceso

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,14 @@ import React from 'react'
 import {createRoot} from 'react-dom/client'
 
 //include your index.scss file into the bundle
-import "../styles/index.css";
+import "../styles/modal.css";
 
 //import your own components
-import Layout from './layout.js'
+import ContactCard from './component/ContactCard'
 
 //
 const root = createRoot(document.querySelector("#app"))
 
 //render your react application
-root.render(<Layout/>)
+root.render(<ContactCard/>)
 
